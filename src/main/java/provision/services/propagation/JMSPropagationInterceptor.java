9103d858ef86a8792c7dad71bd02790795eab891
Marcus Simonsen <msimonsen@blackberry.com>
2013-08-09 09:49:50 -0400

    Unset the propagation context

--- src/main/java/provision/services/propagation/JMSPropagationInterceptor.java
+++ src/main/java/provision/services/propagation/JMSPropagationInterceptor.java
@@ -4,6 +4,8 @@ import javax.interceptor.AroundInvoke;
 import javax.interceptor.InvocationContext;
 import javax.jms.Message;
 
+import provision.services.logging.Logger;
+
 /**
  * Interceptor for JMS MDB consumption, that will add propagation to 
  * local implementation (non-weblogic) version
@@ -15,19 +17,25 @@ public class JMSPropagationInterceptor {
 
 	@AroundInvoke
 	public Object propagate(InvocationContext ctx) throws Exception {
+		PropagationContext pCtx = PropagationContextFactory.get();
+		Object ret = null;
 		
 		try {
 			Object []params = ctx.getParameters();
 			if (params.length ==1 && params[0] instanceof Message) {
 				Message jmsMsg = (Message)params[0];
 				String reqId = jmsMsg.getStringProperty(PropagationContext.REQUEST_ID_KEY);
-				PropagationContextFactory.get().setId(reqId);
+				pCtx.setId(reqId);
+				ret = ctx.proceed();
 			}
 		}catch (Exception e) {
-			//TODO
-			e.printStackTrace();
+			Logger.error(getClass().getName(), "JMS_INTERCEPTOR_ER","msg",e.getMessage(),e);
+		}finally {
+			pCtx.unSetContext();
 		}
-		return ctx.proceed();
+		
+		
+		return ret;
 				
 	}
 }
